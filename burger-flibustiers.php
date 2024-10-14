343a262d1e70dac4f2eadb97a46f6bcded62763c
ClementFlib <clement@les-flibustiers.fr>
2024-10-14 08:44:49 +0200

    Ajout de PUC pour maj auto (fix namespaces)

--- burger-flibustiers.php
+++ burger-flibustiers.php
@@ -7,7 +7,7 @@ Author: Clément GUEZOU - Développeur web chez Les Flibustiers
 */
 
 // Permet de vérifier la version pour MAJ auto du plugin
-require 'includes/plugin-update-checker/plugin-update-checker.php';
+require plugin_dir_path(__FILE__) . 'includes/plugin-update-checker/plugin-update-checker.php';
 
 // Inclut les fichiers nécessaires
 include plugin_dir_path(__FILE__) . 'includes/settings-register.php';
@@ -21,3 +21,14 @@ function enqueue_burger_flibustiers_assets() {
     wp_enqueue_script('burger-flibustiers-js', plugin_dir_url(__FILE__) . 'js/burger-flibustiers.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'enqueue_burger_flibustiers_assets');
+
+use YahnisElsts\PluginUpdateChecker\v5p4\PucFactory;
+
+$updateChecker = PucFactory::buildUpdateChecker(
+    'https://github.com/ClementFlib/burger-flibustiers-plugin/', // URL du dépôt GitHub
+    __FILE__, // Chemin vers le fichier principal du plugin
+    'burger-flibustiers-plugin' // Slug du plugin
+);
+
+// Définis la branche, ici `master` par défaut.
+$updateChecker->setBranch('master');
