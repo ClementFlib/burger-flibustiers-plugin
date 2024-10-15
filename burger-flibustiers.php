9591ad95e6011c4e8fd81eca2281681a560fcd6a
ClementFlib <clement@les-flibustiers.fr>
2024-10-15 14:16:49 +0200

    v2.2.7

--- burger-flibustiers.php
+++ burger-flibustiers.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Burger Flibustiers
 Description: Plugin pour ajouter un menu burger dynamique avec un shortcode.
-Version: 2.2.6
+Version: 2.2.7
 Author: Clément GUEZOU - Développeur web chez Les Flibustiers
 */
 
