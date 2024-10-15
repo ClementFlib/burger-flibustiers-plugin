48f0a5b456f060d11db6721c5f70135c01babb18
ClementFlib <clement@les-flibustiers.fr>
2024-10-15 13:10:34 +0200

    adding social media options

--- js/burger-flibustiers.js
+++ js/burger-flibustiers.js
@@ -3,3 +3,25 @@ document.getElementById('burger-menu-icon').addEventListener('click', function()
 	document.getElementById('burger-menu').classList.toggle('open');
 	this.classList.toggle('open');
 });
+
+function addAndRemoveSocialLinks(options){
+	document.addEventListener('DOMContentLoaded', function () {
+		let index = options;
+		const container = document.querySelector('#add-social-link').parentNode;
+
+		// Ajouter un nouveau réseau social
+		document.querySelector('#add-social-link').addEventListener('click', function () {
+			const template = document.querySelector('#new-social-link-template').cloneNode(true);
+			template.style.display = 'block';
+			template.innerHTML = template.innerHTML.replace(/index_placeholder/g, index++);
+			container.insertBefore(template, this);
+		});
+
+		// Supprimer un champ de réseau social
+		container.addEventListener('click', function (event) {
+			if (event.target.classList.contains('remove-social-link')) {
+				event.target.parentNode.remove();
+			}
+		});
+	});
+}
